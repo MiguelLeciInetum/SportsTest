6a4dbd3a88c430c72e98fe527aa5615b81303401
Miguel Leciñena <miguel.lecinena@external.atlascopco.com>
2023-09-15 10:18:23 +0200

    first commit

---
+++ src/main/java/com/example/sportsproyect/SportsProyectApplication.java
@@ -0,0 +1,13 @@
+package com.example.sportsproyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsProyectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportsProyectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sportsproyect/SportsProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sportsproyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsProyectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
