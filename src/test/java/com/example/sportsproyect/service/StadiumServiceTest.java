7e199470a4fa32bbdd995c6867ee2eb28b081c02
Miguel Leciñena <miguel.lecinena@external.atlascopco.com>
2023-10-23 10:00:21 +0200

    feat teams and stadium test service

--- src/test/java/com/example/sportsproyect/service/StadiumServiceTest.java
+++ src/test/java/com/example/sportsproyect/service/StadiumServiceTest.java
@@ -1,42 +1,80 @@
 package com.example.sportsproyect.service;
 
+import com.example.sportsproyect.model.Stadium;
+import com.example.sportsproyect.repository.StadiumRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
 
 class StadiumServiceTest {
-
+    @Mock
+    private StadiumRepository stadiumRepository;
+    private AutoCloseable autoCloseable;
+    private StadiumService underTest;
     @BeforeEach
     void setUp() {
+        autoCloseable = MockitoAnnotations.openMocks(this);
+        underTest = new StadiumService(stadiumRepository);
     }
-
     @AfterEach
-    void tearDown() {
+    void tearDown() throws Exception {
+        autoCloseable.close();
     }
-
     @Test
-    void getAllStadiums() {
+    void canGetAllStadiums() {
+        //when
+        underTest.getAllStadiums();
+        //then
+        verify(stadiumRepository).findAll();
     }
-
     @Test
     void getStadiumById() {
+        Stadium mockedStadium = mock(Stadium.class);
+        when(stadiumRepository.findById(1L)).thenReturn(Optional.of(mockedStadium));
+        Stadium playerById = underTest.getStadiumById(1L);
+        assertThat(playerById).isEqualTo(mockedStadium);
     }
-
     @Test
     void createStadium() {
+        //when
+        Stadium mockedStadium = mock(Stadium.class);
+        underTest.createStadium(mockedStadium);
+        //then
+        verify(stadiumRepository).save(mockedStadium);
     }
-
     @Test
     void updateStadium() {
+        //when
+        Stadium mockedStadium = mock(Stadium.class);
+        underTest.updateStadium(mockedStadium);
+        //then
+        verify(stadiumRepository).save(mockedStadium);
     }
-
     @Test
-    void deleteStadium() {
+    @Disabled
+    void deleteStadium() throws Exception {
+        //when
+        Stadium mockedStadium = mock(Stadium.class);
+        underTest.createStadium(mockedStadium);
+        underTest.deleteStadium(mockedStadium.getId());
+        //then
+        verify(stadiumRepository).delete(mockedStadium);
     }
-
     @Test
     void deleteAllStadiums() {
+        //when
+        underTest.deleteAllStadiums();
+        //then
+        verify(stadiumRepository).deleteAll();
     }
 }
--- src/test/java/com/example/sportsproyect/service/TeamServiceTest.java
+++ src/test/java/com/example/sportsproyect/service/TeamServiceTest.java
@@ -1,42 +1,80 @@
 package com.example.sportsproyect.service;
 
+import com.example.sportsproyect.model.Team;
+import com.example.sportsproyect.repository.TeamRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
 
 class TeamServiceTest {
-
+    @Mock
+    private TeamRepository teamsRepository;
+    private AutoCloseable autoCloseable;
+    private TeamService underTest;
     @BeforeEach
     void setUp() {
+        autoCloseable = MockitoAnnotations.openMocks(this);
+        underTest = new TeamService(teamsRepository);
     }
-
     @AfterEach
-    void tearDown() {
+    void tearDown() throws Exception {
+        autoCloseable.close();
     }
-
     @Test
-    void getAllTeams() {
+    void canGetAllTeams() {
+        //when
+        underTest.getAllTeams();
+        //then
+        verify(teamsRepository).findAll();
     }
-
     @Test
     void getTeamById() {
+        Team mockedTeam = mock(Team.class);
+        when(teamsRepository.findById(1L)).thenReturn(Optional.of(mockedTeam));
+        Team playerById = underTest.getTeamById(1L);
+        assertThat(playerById).isEqualTo(mockedTeam);
     }
-
     @Test
     void createTeam() {
+        //when
+        Team mockedTeam = mock(Team.class);
+        underTest.createTeam(mockedTeam);
+        //then
+        verify(teamsRepository).save(mockedTeam);
     }
-
     @Test
     void updateTeam() {
+        //when
+        Team mockedTeam = mock(Team.class);
+        underTest.updateTeam(mockedTeam);
+        //then
+        verify(teamsRepository).save(mockedTeam);
     }
-
     @Test
-    void deleteTeam() {
+    @Disabled
+    void deleteTeam() throws Exception {
+        //when
+        Team mockedTeam = mock(Team.class);
+        underTest.createTeam(mockedTeam);
+        underTest.deleteTeam(mockedTeam.getId());
+        //then
+        verify(teamsRepository).delete(mockedTeam);
     }
-
     @Test
     void deleteAllTeams() {
+        //when
+        underTest.deleteAllTeams();
+        //then
+        verify(teamsRepository).deleteAll();
     }
 }
